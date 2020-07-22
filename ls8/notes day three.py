84a008fe754d5270b5b31690a08ab16883a7358c
michhottinger <michhottinger@users.noreply.github.com>
2020-07-22 11:27:48 -0700

    added the pop push but not tested

--- ls8/notes day three.py
+++ ls8/notes day three.py
@@ -85,9 +85,7 @@ class CPU:
             0b01000111 : "PRN",
             0b00000001 : "HLT",
             0b10000010 : "LDI",
-            0b10100010 : "MUL",
-            0b01000101 : "PUSH",
-            0b01000110 : "POP"
+            0b10100010 : "MUL"
         }
         
 
@@ -187,7 +185,6 @@ class CPU:
             ir = self.ram_read(self.pc) # Instruction Register, contains a copy of the currently executing instruction
             if ir in self.branch_table:
                 self.branch_table[ir]
-            
             if ir in self.branch_table and self.branch_table[ir] == "HLT":
                 running = self.HLT()
                
@@ -203,37 +200,6 @@ class CPU:
                 elif self.branch_table[ir] == "PRN":
                     self.PRN(operand_a)
                 
-                elif self.branch_table[ir] == "PUSH":
-                    sp = 7
-                    register[sp] = 0xff
-                    register[sp] -= 1
-    
-                    register[sp] &= 0xff#keeps r7 in the 
-    
-                    #get register number
-                    reg_num = memory[pc+1]
-                    value = register[reg_num]
-    
-                    #store in memory
-                    address_to_push_to = register[sp]
-                    memory[address_to_push_to] = value
-    
-                    pc += 2
-                
-                elif self.branch_table[ir] == "POP":
-                    #get value from RAM
-                    address_to_pop_from = register[sp]
-                    value = memory[address_to_pop_from]
-        
-                    #store in the given registery
-                    reg_num = memory[pc + 1]
-                    register[reg_num] = value
-        
-                    #increment SP
-                    register[sp] +=1
-        
-                    pc +=2
-                
                 else: 
                     op = self.branch_table[ir]
                     self.alu(op, operand_a, operand_b)
@@ -247,3 +213,83 @@ class CPU:
                 self.pc += 3
             else:
                 self.pc += 2
+                
+                
+                
+                
+ """ 
+CPU stack
+push and pop instructions to use with the stack
+components of a stack?
+--push a thing on the stack, then store it
+--store items in RAM is an option
+--store in registers is another option but they are used for other thigs
+register = [0] * 8
+
+pc = 0  # Program Counter, index into memory of the current instruction
+# AKA a pointer to the current instruction
+
+fl = 0
+sp = 7
+register[sp] = 0xff
+
+running = True
+
+while running:
+    inst = memory[pc]
+
+    if inst == 1:  # PRINT_BEEJ
+        print("Beej")
+        pc += 1
+
+    elif inst == 2:  # HALT
+        running = False
+
+    elif inst == 3:  # SAVE_REG
+        reg_num = memory[pc + 1]
+        value = memory[pc + 2]
+
+        register[reg_num] = value
+
+        pc += 3
+
+    elif inst == 4: # PRINT_REG
+        reg_num = memory[pc + 1]
+        print(register[reg_num])
+
+        pc += 2
+        
+    elif inst == 5:#push
+    #decriment stack pointer
+    register[sp] -= 1
+    
+    register[sp] &= 0xff#keeps r7 in the 
+    
+    #get register number
+    reg_num = memeory[pc+1]
+    value = register[reg_num]
+    
+    #store in memory
+    address_to_push_to = register[sp]
+    memeory [address_to_push_to] = value
+    
+    pc += 2
+    
+    elif inst ==6:
+    #get value from RAM
+        address_to_pop_from = register[sp]
+        value = memory[addres_to_pop_from]
+        
+        #store in the given registery
+        reg_num = memory[pc + 1]
+        register[reg_num] = value
+        
+        #increment SP
+        register[sp] +=1
+        
+        pc +=2
+
+    else:
+        print(f"Unknown instruction {inst}")
+        
+"""
